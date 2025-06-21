903900d1eaffaf6070698a429f6c6944482430ca
Vaden43008 <vaden43008@gmail.com>
2025-06-21 12:39:34 -0700

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,113 @@
+import random
+import requests
+from moviepy.editor import *
+
+# Function to add video source
+def add_video_source(video_path, source_path):
+    # Use moviepy to add video source
+    pass
+
+# Function to add audio sound
+def add_audio_sound(video_path, audio_path):
+    # Use moviepy to add audio sound
+    pass
+
+# Function to add audio music
+def add_audio_music(video_path, music_path):
+    # Use moviepy to add audio music
+    pass
+
+# Function to add image or GIF
+def add_image_or_gif(video_path, image_path):
+    # Use moviepy to add image or GIF
+    pass
+
+# Function to add online audio sound
+def add_online_audio_sound(video_path, audio_url):
+    # Download audio from URL
+    # Use moviepy to add online audio sound
+    pass
+
+# Function to add online music audio
+def add_online_music_audio(video_path, music_url):
+    # Download music from URL
+    # Use moviepy to add online music audio
+    pass
+
+# Function to add online archive.org open source audio
+def add_archive_org_audio(video_path, audio_query):
+    # Use archive.org API to search for audio
+    # Download audio
+    # Use moviepy to add archive.org open source audio
+    pass
+
+# Function to add online open source movie archive.org
+def add_archive_org_movie(video_path, movie_query):
+    # Use archive.org API to search for movies
+    # Download movie
+    # Use moviepy to add archive.org open source movie
+    pass
+
+# Function to add online open source cartoon videos from archive.org
+def add_archive_org_cartoon(video_path, cartoon_query):
+    # Use archive.org API to search for cartoon videos
+    # Download cartoon video
+    # Use moviepy to add archive.org open source cartoon video
+    pass
+
+# Function to add YouTube random video generator before a certain year
+def add_youtube_random_video(video_path, year):
+    # Use YouTube API to search for random videos before the specified year
+    # Download video
+    # Use moviepy to add YouTube random video
+    pass
+
+# Function to add online Facebook random video generator before a certain year
+def add_facebook_random_video(video_path, year):
+    # Use Facebook API or web scraping to find random videos before the specified year
+    # Download video
+    # Use moviepy to add Facebook random video
+    pass
+
+# Function to add online green screen a generate on YTP Generate
+def add_green_screen(video_path):
+    # Use YTP Generate API or library to generate green screen effect
+    pass
+
+# Function to generate chaos YouTube poop
+def generate_chaos_ytp(video_path):
+    # Use YTP Generate API or library to generate chaos YouTube poop
+    pass
+
+# Function to generate video remix
+def generate_video_remix(video_paths):
+    # Use moviepy to remix videos
+    pass
+
+# Function to generate YTP super chaos
+def generate_super_chaos_ytp(video_paths):
+    # Use YTP Generate API or library to generate super chaos YouTube poop
+    pass
+
+# Main function
+def main():
+    video_path = "input_video.mp4"
+    # Example usage of functions
+    add_video_source(video_path, "source_video.mp4")
+    add_audio_sound(video_path, "sound.mp3")
+    add_audio_music(video_path, "music.mp3")
+    add_image_or_gif(video_path, "image.gif")
+    add_online_audio_sound(video_path, "audio_url")
+    add_online_music_audio(video_path, "music_url")
+    add_archive_org_audio(video_path, "audio_query")
+    add_archive_org_movie(video_path, "movie_query")
+    add_archive_org_cartoon(video_path, "cartoon_query")
+    add_youtube_random_video(video_path, 2008)
+    add_facebook_random_video(video_path, 2012)
+    add_green_screen(video_path)
+    generate_chaos_ytp(video_path)
+    generate_video_remix(["video1.mp4", "video2.mp4"])
+    generate_super_chaos_ytp(["video1.mp4", "video2.mp4"])
+
+if __name__ == "__main__":
+    main()
